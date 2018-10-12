156ee35636bcfcdf39963a8c26d98009c162e117
shazibshaikh <mailshazib@gmail.com>
2018-10-12 15:39:00 +0500

    initial setup of djtest without any apps

--- djtest/settings.py
+++ djtest/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djtest',
-        'USER': 'root@localhost',
+        'USER': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     },
