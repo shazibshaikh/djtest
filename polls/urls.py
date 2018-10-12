a255e6923da3d77ececebe20241efbf82ecf1910
shazibshaikh <mailshazib@gmail.com>
2018-10-12 17:33:53 +0500

    First database migration and models class

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,4 @@
-"""djtest URL Configuration
+"""polls URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
