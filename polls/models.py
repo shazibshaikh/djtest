5afbc3850cad57bb962be01533937c78cb58dd50
shazibshaikh <mailshazib@gmail.com>
2018-10-15 19:02:16 +0500

    Included django_mysql test of ListCharField model

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,29 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+from django.db.models import CharField, Model
+from django_mysql.models import ListCharField, ListTextField
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
 
+class Person(Model):
+    name = CharField(max_length=30)
+    post_nominals = ListCharField(
+        base_field=CharField(max_length=10),
+        size=6,
+        max_length=(6 * 11) #6 * 10 character nominals, plus commas
+    )
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+"""
+Polls app view file
+"""
+from django.shortcuts import render, get_object_or_404
+#from django.template import loader
+from django.http import HttpResponse, Http404
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Assalaamu Alaikum.  Welcome to POLL INDEX <B>MR. MUSA</b>!!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    q =get_object_or_404(Question, pk=question_id,)
+    return render(request, 'polls/detail.html', {'question':q})
+
+def results(request, question_id):
+    return HttpResponse("Results of Question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting on Question %s." % question_id)
